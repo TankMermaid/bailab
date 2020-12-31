6d5ad36f8fcb546e36f47d4e1ef50838fceff692
alvin <tanklovemermaid@gmail.com>
2020-12-31 16:17:30 +0800

    infor

---
+++ demo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#########################################################################
+# File Name: demo.py
+# Author: Xiao-Ning Tank Zhang
+# Email: xnzhang@genetics.ac.cn
+# Created Time: Thu Dec 31 16:16:28 2020
+#########################################################################
+import os
+
+import sys
